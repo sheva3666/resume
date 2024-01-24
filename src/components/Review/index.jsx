c9925de5b494e12a6dceaf0f9030be0d6f44a0ef
sheva3666 <ivan.shevchenko@sap.com>
2024-01-24 16:38:23 +0100

    refactor Review section

--- src/components/Review/index.jsx
+++ src/components/Review/index.jsx
@@ -2,18 +2,19 @@ import Text from "../common/Text";
 import TitleOne from "../common/TitleOne";
 import "./index.css";
 
-const Review = () => {
-  return (
-    <div className="review">
-      <h2 className="review__title">I’m Shevchenko Ivan</h2>
-      <TitleOne title="Frontend Developer" />
-      <Text maxwidth={960}>
-        I would like to become a part of a friendly and professional team, in a
-        moment I would develop my skills in the field of research and
-        development, in order to achieve team results.
-      </Text>
-    </div>
-  );
+const text = {
+  name: "I’m Shevchenko Ivan",
+  title: "Frontend Developer",
+  description:
+    "I would like to become a part of a friendly and professional team, in a moment I would develop my skills in the field of research and development, in order to achieve team results.",
 };
+
+const Review = () => (
+  <div className="review">
+    <h2 className="review__title">{text.name}</h2>
+    <TitleOne title={text.title} />
+    <Text maxwidth={960}>{text.description}</Text>
+  </div>
+);
 
 export default Review;
